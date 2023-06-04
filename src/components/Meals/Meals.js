9cc2704758bc1eb511d226a2445bc4bb5ef9275b
Nishant Sinha <nishantsinha1197@gmail.com>
2023-06-04 14:11:45 +0530

    My first app

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailabeMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+export default Meals;
